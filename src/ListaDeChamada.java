6e27799545cdf222b61c33916d51a9137b37bbbe
Karen <karen.sn.amb@gmail.com>
2023-04-02 20:37:48 -0300

    realizando o loop da chamada

--- src/ListaDeChamada.java
+++ src/ListaDeChamada.java
@@ -1,72 +1,62 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class ListaDeChamada {
     public static void main(String[] args) {
        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite o número de alunos na turma: ");
+        int numAlunos = input.nextInt();
         ArrayList<String> alunos = new ArrayList<String>();
 
-        // Adiciona os nomes dos alunos à lista
-       System.out.println("Digite a lista de alunos  (digite 'fim' para parar):");
-       String nomeAluno = scanner.nextLine();
-        while (!nomeAluno.equalsIgnoreCase("fim")) {
-            alunos.add(nomeAluno);
-            nomeAluno = scanner.nextLine();
+        int[] faltas = new int[numAlunos];
+
+
+
+        for (int i = 0; i < numAlunos; i++) {
+            System.out.print("Digite o nome do aluno: ");
+            String nomes = scanner.next();
+            alunos.add(nomes);
+            faltas[i] = 0;
         }
 
-        // Realizando a chamada
-        //data de input
 
-        int presenca = 0, presenca2 = 0;
-        String data1 = new String("dd/MM/yyyy");
-        System.out.println("Digite a data da chamada na semana:");
-        data1 = scanner.nextLine();
 
-        System.out.println("Digite o nome dos alunos presentes (digite 'fim' para parar):");
-        nomeAluno = scanner.nextLine();
-        while (!nomeAluno.equalsIgnoreCase("fim")) {
-            if (alunos.contains(nomeAluno)) {
-                System.out.println(nomeAluno + " está presente.");
-                presenca = 1;
-
-            } else {
-                System.out.println("Não há um aluno com esse nome.");
-            }
-            nomeAluno = scanner.nextLine();
+        LocalDate dataAtual = LocalDate.now();
+        System.out.println("Data atual: " + dataAtual);
+        int contFaltas = 0;
+       List<Integer> falta = new ArrayList<>();
+        System.out.println("Quantas faltas o aluno teve na semana?");
+        int falt;
+        for (int i = 0; i < numAlunos; i++) {
+            System.out.print(alunos.get(i) + ": ");
+            falt = Integer.parseInt(scanner.next());
+            falta.add(falt);
+            contFaltas = contFaltas + falta.get(i);
         }
 
-        String data2 = new String("dd/MM/yyyy");
-        System.out.println("Digite a data da chamada na semana:");
-        data2 = scanner.nextLine();
-
-        System.out.println("Digite o nome dos alunos presentes (digite 'fim' para parar):");
-        nomeAluno = scanner.nextLine();
-        while (!nomeAluno.equalsIgnoreCase("fim")) {
-            if (alunos.contains(nomeAluno)) {
-                System.out.println(nomeAluno + " está presente.");
-                presenca2 = presenca + 1;
-                for (String name : alunos) {
-                    System.out.println("O aluno:" + nomeAluno + " " + "esteve presente: " + presenca2 + " dia(s) da semana");
-                }
 
 
-            } else {
-                System.out.println("Não há um aluno com esse nome.");
-            }
-            nomeAluno = scanner.nextLine();
-
-
+        // Exibe a lista de alunos e suas faltas
+        System.out.println("Lista de alunos e faltas:");
+        for (int i = 0; i < numAlunos; i++) {
+            System.out.println(alunos.get(i) + " - " + falta.get(i));
         }
+
+}
+
 
 // Fechando o scanner
-        scanner.close();
+       // scanner.close();
 
     }
 
-        //removendo o nome do aluno para não haver duplicação, já que tem-se apenas 2 aulas
-        //alunos.remove(nomeAluno);
 
-}
+
+
 
 
 
--- src/NomeAlunos.java
+++ src/NomeAlunos.java
@@ -2,15 +2,15 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 //public class NomeAlunos {
-    //Scanner scanner = new Scanner(System.in);
-    //ArrayList<String> alunos = new ArrayList<String>();
-
-    // Adiciona os nomes dos alunos à lista
-       // System.out.println("Digite a lista de alunos  (digite 'fim' para parar):");
-    //String nomeAluno = scanner.nextLine();
-        //while (!nomeAluno.equalsIgnoreCase("fim")) {
-        //alunos.add(nomeAluno);
-       // nomeAluno = scanner.nextLine();
-    //}
+//    Scanner scanner = new Scanner(System.in);
+//    ArrayList<String> alunos = new ArrayList<String>();
+//
+//     Adiciona os nomes dos alunos à lista
+//        System.out.println("Digite a lista de alunos  (digite 'fim' para parar):");
+//    String nomeAluno = scanner.nextLine();
+//        while (!nomeAluno.equalsIgnoreCase("fim")) {
+//        alunos.add(nomeAluno);
+//        nomeAluno = scanner.nextLine();
+ //  }
 
 //}
